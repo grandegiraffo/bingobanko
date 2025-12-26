6284b45d8759065a43678f15380ba704b901ea3e
Kasper Duhn <2128159+grandegiraffo@users.noreply.github.com>
2025-12-26 14:41:04 +0100

    Remove unused import afterEach :rotating_light:

--- src/components/bingo-game.spec.ts
+++ src/components/bingo-game.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeEach, vi, afterEach } from 'vitest'
+import { describe, it, expect, beforeEach, vi } from 'vitest'
 import { mount } from '@vue/test-utils'
 import BingoGame from './bingo-game.vue'
 
